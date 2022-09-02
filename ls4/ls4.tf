521d6616efc418ef7a03c7fca61eb11c295a77d1
Sergey <Sergey@bk.ru>
2022-09-02 17:39:59 +0000

    Add LifeCyrcle

---
+++ ls4/ls4.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_eip" "static_ip" {
+  instance = "${aws_instance.AWS_web_server.id}"
+  vpc = true
+}
+resource "aws_instance" "AWS_web_server" {
+  ami                    = "ami-05fa00d4c63e32376"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.For_Linux.id]
+  user_data              = templatefile("scriptaws.sh.tpl", {
+    f_name = "Sergey"
+    l_name = "Nomad"
+    names = ["Ivan","Vera","Slava","11111111111"]
+  })
+  tags = {
+    Name = "Amazon Server by Terraform"
+    Owner = "Nomad"
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+resource "aws_security_group" "For_Linux" {
+  name        = "Linux_security"
+  description = "LS4_security"
+  vpc_id = "vpc-2f563b52"
+  dynamic "ingress" {
+    for_each = ["80","443"]
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
---
+++ ls4/scriptaws.sh.tpl
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo yum -y update
+sudo yum -y install httpd
+myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
+cat <<EOF >/var/www/html/index.html
+<html>
+<h2>Bild by Terraform <font color="red"> v1.2.8</font></h2><br>
+Owner ${f_name} ${l_name} $myip<br>
+%{ for x in names ~}
+Hello to ${x} from ${l_name}<br>
+%{ endfor ~}
+</html>
+EOF
+sudo service httpd restart
+sudo chkconfig httpd on
